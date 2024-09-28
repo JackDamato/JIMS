753361138e949d006a56ca20089ed5a3ef6fbd4b
JackDamato <100488603+JackDamato@users.noreply.github.com>
2024-09-28 05:16:03 +0000

    Created Datebase Commit Functions for Users and Activities

---
+++ myenv/MongoDB/data-functions.py
@@ -0,0 +1,42 @@
+from pymongo_get_database import get_database
+from datetime import date
+from datetime import datetime
+
+user = {
+  "Username" : "",
+  "Age" : 18,
+  "Email" : "",
+  "Ethnicity" : "",
+  "Gender" : "",
+  "Interests" : "",
+  "Major" : "",
+  "Name" : "",
+  "Organizations" : "",
+  "Password" : "",
+  "School" : "",
+  "Year" : 1
+}
+
+
+activity = {
+    "Creator" : "",
+    "Title" : "",
+    "Date" : str(date.today()),
+    "Time" : str(datetime.now().time()),
+    "Description" : "",
+    "Tags" : ""
+}
+
+def commit_user():
+    db = get_database()
+    users = db['users']
+    users.insert_one(user)
+
+
+def commit_activity():
+    db = get_database()
+    activities = db['activities']
+    activities.insert_one(activity)
+
+
+commit_activity()
--- myenv/MongoDB/pymongo_get_database.py
+++ myenv/MongoDB/pymongo_get_database.py
@@ -1,11 +1,5 @@
 from pymongo import MongoClient
 def get_database():
-    CONNECTION_STRING = "mongodb+srv://mangia3:linked@linked-cluster.afu0s.mongodb.net/"
-
+    CONNECTION_STRING = "mongodb+srv://mangia3:linked@linked-cluster.afu0s.mongodb.net/linked_database"
     client = MongoClient(CONNECTION_STRING)
-
-    print(client)
-    return True
-
-
-get_database()
+    return client["linked_database"]
